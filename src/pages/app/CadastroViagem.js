d06e3814335e2f3487969fc2b6f712e7aa0ff3c9
Otávio Augusto Lorenzatto <lorenzattoo@gmail.com>
2024-11-09 15:57:34 -0300

    Adicionado função para apagar dados inseridos pelo usuario na tela de login ao ele trocar para a tela de cadastro e voltar para a tela de Login; Adicionado funções para a tela de CadastroViagem; Adicionado calendarios na tela de CadastroViagem e borão de adicionar viagem

--- src/pages/app/CadastroViagem.js
+++ src/pages/app/CadastroViagem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   View,
   TextInput,
@@ -9,19 +9,31 @@ import {
   Text,
   Keyboard,
 } from "react-native";
-import ViagemService from "../../services/ViagemService"; 
+import { Calendar, LocaleConfig } from "react-native-calendars";
+import { ptBR } from "../../utils/estilocalendario";
+import ViagemService from "../../services/ViagemService";
+
 import flechaesquerda from "../../../assets/flechaesquerda.png";
 import logo from "../../../assets/logo.png";
 import calendarioida from "../../../assets/calendarioida.png";
+import calendariovolta from "../../../assets/calendariovolta.png";
+import marcacaomapa from "../../../assets/marcacaomapa.png";
 
+LocaleConfig.locales["pt-br"] = ptBR;
+LocaleConfig.defaultLocale = "pt-br";
+
+// Receba `navigation` como uma prop
 export default function CadastroViagem({ navigation }) {
-  const { acaoCalendario } = ViagemService(); 
+  const service = ViagemService();
 
   return (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    <TouchableWithoutFeedback onPress={service.ocultarTecladoECalendario}>
       <View style={styles.container}>
         <View style={styles.header}>
-          <TouchableOpacity onPress={() => navigation.goBack()} style={styles.flechaContainer}>
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={styles.flechaContainer}
+          >
             <Image
               source={flechaesquerda}
               style={[styles.flecha, { tintColor: "#FFFF" }]}
@@ -33,22 +45,82 @@ export default function CadastroViagem({ navigation }) {
         <View style={styles.tituloContainer}>
           <TextInput
             style={styles.tituloInput}
-            placeholder="Titulo da viagem:"
+            value={service.tituloViagem}
+            placeholder="Título da viagem:"
             placeholderTextColor="#888"
+            onChangeText={service.setTituloViagem}
           />
         </View>
 
         <View style={styles.datas}>
           <TouchableOpacity
-            onPress={() => acaoCalendario("ida")}
+            onPress={() => service.acaoCalendario("ida")}
             style={styles.dataContainer}
           >
             <Image source={calendarioida} style={styles.iconecalendario} />
             <Text style={styles.textoDatas}>
-            
+            {service.formatarData(service.dataIda, "ida")}
             </Text>
           </TouchableOpacity>
+
+          {service.calendarioVisivel.ida && (
+            <Calendar
+              style={styles.calendario}
+              theme={styles.temacalendario}
+              minDate={new Date().toDateString()}
+              onDayPress={(day) =>
+                service.salvaDataIda(new Date(day.dateString))
+              }
+            />
+          )}
+
+          <TouchableOpacity
+            onPress={() => service.acaoCalendario("volta")}
+            style={styles.dataContainer}
+          >
+            <Image source={calendariovolta} style={styles.iconecalendario} />
+            <Text style={styles.textoDatas}>
+            {service.formatarData(service.dataVolta, "volta")}
+            </Text>
+          </TouchableOpacity>
+
+          {service.calendarioVisivel.volta && (
+            <Calendar
+              style={styles.calendario}
+              theme={styles.temacalendario}
+              minDate={new Date().toDateString()}
+              onDayPress={(day) =>
+                service.salvaDataVolta(new Date(day.dateString))
+              }
+            />
+          )}
         </View>
+
+        <View style={styles.cidadeContainer}>
+          <Image source={marcacaomapa} style={styles.iconecalendario} />
+          <Text style={styles.cidadeTexto}>
+            {service.cidade ? service.cidade : "Nenhuma cidade selecionada"}
+          </Text>
+        </View>
+
+        <View style={styles.gastoContainer}>
+            <Text style={styles.gastoLabel}>Gasto previsto:</Text>
+            <TextInput
+              style={styles.inputGasto}
+              value={service.gastoPrevisto}
+              onChangeText={service.formataValorInserido}
+              keyboardType="numeric"
+              placeholderTextColor="#FFFF"
+            />
+        </View>
+
+          <TouchableOpacity
+            style={styles.botaoAdicionar}
+            onPress={service.addViagemDadosSalvos}
+          >
+            <Text style={styles.textoBotao}>+ Adicionar</Text>
+          </TouchableOpacity>
+
       </View>
     </TouchableWithoutFeedback>
   );
@@ -57,7 +129,7 @@ export default function CadastroViagem({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#00050D',
+    backgroundColor: "#00050D",
   },
   header: {
     flexDirection: "row",
@@ -107,5 +179,46 @@ const styles = StyleSheet.create({
   textoDatas: {
     color: "#fff",
     fontSize: 16,
+  },
+  cidadeContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  cidadeTexto: {
+    color: "#FFFFFF",
+    fontSize: 18,
+    marginLeft: 10,
+  },
+  cidadeNegrito: {
+    fontWeight: "bold",
+  },
+  gastoContainer: {
+    marginTop: 50,
+  },
+  gastoLabel: {
+    color: "#FFFFFF",
+    fontSize: 18,
+  },
+  inputGasto: {
+    color: "#999",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  botaoAdicionar: {
+    backgroundColor: "#0E6EFF",
+    borderRadius: 40,
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    alignItems: "center",
+    alignSelf: "center",
+    marginTop: 120,
+    marginBottom: 40,
+    marginLeft: 140,
+  },
+  textoBotao: {
+    color: "#FFFFFF",
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
--- src/services/ViagemService.js
+++ src/services/ViagemService.js
@@ -1,12 +1,19 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Keyboard } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const ViagemService = (fechar) => {
-  const [calendarioVisivel, setCalendarioVisivel] = useState(false);
-  const [selecionaCalendario, setSelecionaCalendario] = useState("");
+  const [calendarioVisivel, setCalendarioVisivel] = useState({
+    ida: false,
+    volta: false,
+  });
+  const [tituloViagem, setTituloViagem] = useState("");
+  const [dataIda, setDataIda] = useState(null);
+  const [dataVolta, setDataVolta] = useState(null);
+  const [cidade, setCidade] = useState("");
+  const [gastoPrevisto, setGastoPrevisto] = useState("R$ 0,00");
 
-  //Função para salvar a cidade e coordenadas
+  // Salva a cidade no AsyncStorage
   const salvarCidade = async (cidadeSelecionada) => {
     if (!cidadeSelecionada) {
       console.log("Nenhuma cidade selecionada");
@@ -14,41 +21,178 @@ const ViagemService = (fechar) => {
     }
 
     try {
-      //Pegando latitude e longitude e nome da cidade
       const { lat, lng } = cidadeSelecionada.geometry.location;
-      const nomeCidade =
+      // Salva a descrição completa da cidade (nome e país)
+      const descricaoCidade =
         cidadeSelecionada.description || cidadeSelecionada.formatted_address;
-      //description faz pegar nome da cidade e país e formatted_address pega outros detralhes do endereço (hotel, rua, etc..)
 
-      console.log(`Latitude: ${lat}, Longitude: ${lng}`);
-
-      //Salva cidade e coordenadas no async
-      await AsyncStorage.setItem("@nomeCidade", nomeCidade);
+      await AsyncStorage.setItem(
+        "@nomeCidade",
+        JSON.stringify({ descricao: descricaoCidade })
+      );
+      setCidade(descricaoCidade);
       await AsyncStorage.setItem(
         "@coordenadasCidade",
         JSON.stringify({ lat, lng })
       );
 
-      console.log(`Cidade ${nomeCidade} salva com sucesso`);
+      console.log(`Cidade ${descricaoCidade} salva com sucesso`);
+      console.log(`Latitude: ${lat}, Longitude: ${lng}`);
 
-      //Fecha modal
       fechar?.();
     } catch (error) {
       console.log("Erro ao salvar a cidade e coordenadas", error);
     }
   };
 
-  //Exibe e esconde o calendario ao clicar nos calendários
-  const acaoCalendario = (type) => {
-    if (selecionaCalendario === type && calendarioVisivel) {
-      setCalendarioVisivel(false);
-    } else {
-      setSelecionaCalendario(type);
-      setCalendarioVisivel(true);
+  // Puxa a cidade do Async
+  const puxarCidade = async () => {
+    try {
+      const cidadeArmazenada = await AsyncStorage.getItem("@nomeCidade");
+      if (cidadeArmazenada) {
+        const cidadeObj = JSON.parse(cidadeArmazenada);
+        const descricaoCidade = cidadeObj.descricao || "Cidade desconhecida";
+        setCidade(descricaoCidade);
+      } else {
+        console.log("Nenhuma cidade armazenada encontrada");
+      }
+    } catch (error) {
+      console.log("Erro ao obter a cidade:", error);
     }
   };
 
-  return { salvarCidade, acaoCalendario };
+  // Função para obter cidade armazenada no AsyncStorage (chamada no useEffect)
+  const obterCidade = async () => {
+    try {
+      const cidadeArmazenada = await AsyncStorage.getItem("@nomeCidade");
+      if (cidadeArmazenada) {
+        setCidade(cidadeArmazenada);
+      }
+    } catch (error) {
+      console.log("Erro ao obter a cidade:", error);
+    }
+  };
+
+  // Função para exibir a data no formato correto
+const formatarData = (date, tipo) => {
+  if (!date) {
+    return tipo === "ida" ? "Data de ida" : "Data de volta";
+  }
+  return new Date(date.getTime() + date.getTimezoneOffset() * 60000).toLocaleDateString("pt-BR");
+};
+
+
+  // Carrega a cidade automaticamente ao inicializar a service
+  useEffect(() => {
+    puxarCidade();
+  }, []);
+
+  // Exibe ou esconde o calendário
+  const acaoCalendario = (tipo) => {
+    setCalendarioVisivel((prev) => ({
+      ida: tipo === "ida" ? !prev.ida : false,
+      volta: tipo === "volta" ? !prev.volta : false,
+    }));
+  };
+
+  // Função para salvar a data de ida no async
+  const salvaDataIda = async (dataIdaSelecionada) => {
+    setDataIda(dataIdaSelecionada);
+    try {
+      await AsyncStorage.setItem("@dataIda", dataIdaSelecionada.toISOString());
+      console.log("Data de ida salva com sucesso:", dataIdaSelecionada);
+    } catch (error) {
+      console.log("Erro ao salvar a data de ida:", error);
+    }
+  };
+
+  // Função para salvar a data de volta no async
+  const salvaDataVolta = async (dataVoltaSelecionada) => {
+    setDataVolta(dataVoltaSelecionada);
+    try {
+      await AsyncStorage.setItem(
+        "@dataVolta",
+        dataVoltaSelecionada.toISOString()
+      );
+      console.log("Data de volta salva com sucesso:", dataVoltaSelecionada);
+    } catch (error) {
+      console.log("Erro ao salvar a data de volta:", error);
+    }
+  };
+
+  //Oculta o teclado e calendarios ao clicar fora do campo
+  const ocultarTecladoECalendario = () => {
+    Keyboard.dismiss();
+    if (calendarioVisivel.ida || calendarioVisivel.volta) {
+      setCalendarioVisivel({ ida: false, volta: false });
+    }
+  };
+
+  // Formatação da cidade para exibição
+  const cidadeFormatada = cidade
+    ? `${cidade.description || cidade.formatted_address}, ${
+        cidade.structured_formatting?.secondary_text || ""
+      }`
+    : "Nenhuma cidade selecionada";
+
+  // Função que formata o valor inserido pelo usuário
+  const formataValorInserido = (value) => {
+    setGastoPrevisto(formataMoeda(value));
+  };
+
+  // Função para adicionar viagem e salvar os dados
+  const addViagemDadosSalvos = async () => {
+    try {
+      const resultado = await cadastroViagemUseCase.salvarViagem({
+        titulo,
+        dataIda,
+        dataVolta,
+        gastoPrevisto,
+        cidade,
+      });
+
+      if (resultado.success) {
+        navigation.navigate("HomecomViagem"); // Navega para a tela "homecomviagem" após o sucesso
+      } else {
+        alert(resultado.message); // Mostra a mensagem de erro caso algo dê errado
+      }
+    } catch (error) {
+      console.log("Erro ao adicionar viagem:", error);
+    }
+  };
+
+  // Função para formatar o valor como moeda
+  const formataMoeda = (value) => {
+    let numericValue = value.replace(/\D/g, "");
+    if (numericValue.length === 0) {
+      numericValue = "0";
+    }
+    numericValue = (numericValue / 100).toFixed(2).replace(".", ",");
+    numericValue = numericValue.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    return `R$ ${numericValue}`;
+  };
+
+  return {
+    tituloViagem,
+    setTituloViagem,
+    dataIda,
+    dataVolta,
+    cidade,
+    setCidade,
+    calendarioVisivel,
+    acaoCalendario,
+    formatarData,
+    salvaDataIda,
+    salvaDataVolta,
+    ocultarTecladoECalendario,
+    salvarCidade,
+    obterCidade,
+    puxarCidade,
+    cidadeFormatada,
+    formataValorInserido,
+    gastoPrevisto,
+    addViagemDadosSalvos,
+  };
 };
 
 export default ViagemService;
